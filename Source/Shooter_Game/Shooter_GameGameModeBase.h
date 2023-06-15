b1a179e8c8ffff9d40b24aeb46fd4912033f48ce
Junghyeon <sie08357@naver.com>
2023-06-15 19:17:02 +0900

    초기 커밋.

---
+++ Source/Shooter_Game/Shooter_GameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Shooter_GameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_GAME_API AShooter_GameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
